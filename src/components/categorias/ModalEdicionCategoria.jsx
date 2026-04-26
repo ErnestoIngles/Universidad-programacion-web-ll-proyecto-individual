3b32aeda014776cbe0e10a898a03f424167fd6a7
Ernesto José Sevilla Inglés <estudianteernestosevilla@gmail.com>
2026-04-26 11:13:30 -0600

    Implementación de edición y eliminación en entidad categoría. (#10)
    * feat(ui): agregar formulario modal edicion de categorias
    
    * feat(ui): implementar funcionalidad de edición de categoría
    
    * agregar modal de eliminar correctamente

--- src/components/categorias/ModalEdicionCategoria.jsx
+++ src/components/categorias/ModalEdicionCategoria.jsx
@@ -1,14 +1,5 @@
-import React, {useState} from "react";
-import { Modal, Form, Button, ModalTitle, ModalBody } from "react-bootstrap";
-
-const [deshabilitado, setDeshabilitado] = useState(false);
-
-    const handleActualizar = async () => {
-      if (deshabilitado) return;
-      setDeshabilitado(true);
-      await actualizarCategoria();
-      setDeshabilitado(false);
-    };
+import React, { useState } from "react";
+import { Modal, Form, Button, ModalTitle, ModalBody, FormGroup, FormLabel, FormControl, ModalFooter } from "react-bootstrap";
 
 const ModalEdicionCategoria = ({
   mostrarModalEdicion,
@@ -16,7 +7,16 @@ const ModalEdicionCategoria = ({
   categoriaEditar,
   manejoCambioInputEdicion,
   actualizarCategoria,
-}) =>{
+}) => {
+
+  const [deshabilitado, setDeshabilitado] = useState(false);
+
+  const handleActualizar = async () => {
+    if (deshabilitado) return;
+    setDeshabilitado(true);
+    await actualizarCategoria();
+    setDeshabilitado(false);
+  };
 
   return (
     <Modal
@@ -28,7 +28,44 @@ const ModalEdicionCategoria = ({
       <ModalTitle>Editar Categoría</ModalTitle>
       <ModalBody>
         <Form>
+
+          <FormGroup className="mb-3">
+            <FormLabel>Nombre</FormLabel>
+            <FormControl
+              type="text"
+              name="nombre_categoria"
+              value={categoriaEditar.nombre_categoria}
+              onChange={manejoCambioInputEdicion}
+              placeholder="Ingresa el nombre de la categoría"
+            />
+          </FormGroup>
+
+          <FormGroup className="mb-3">
+            <FormLabel>Descripción</FormLabel>
+            <FormControl
+              as="textarea"
+              name="descripcion_categoria"
+              value={categoriaEditar.descripcion_categoria}
+              onChange={manejoCambioInputEdicion}
+              placeholder="Ingresa una descripción para la categoría"
+            />
+          </FormGroup>
+
+        </Form>
       </ModalBody>
+
+      <ModalFooter>
+        <Button variant="secondary" onClick={() => SetMostrarModalEdicion(false)}>
+          Cancelar edición
+        </Button>
+        <Button
+          variant="primary"
+          onClick={handleActualizar}
+          disabled={categoriaEditar.nombre_categoria.trim() === "" || deshabilitado}
+        >
+          Actualizar
+        </Button>
+      </ModalFooter>
     </Modal>
 
   );
